33dc02a57d93a705619ec0835462b5bb0557907e
Acespace <alston.clark17@gmail.com>
2013-11-16 11:28:36 -0500

    Create group5bsearch.py
    Group5

---
+++ group5bsearch.py
@@ -0,0 +1,39 @@
+
+
+#Group Members 
+#Alston Clark
+
+
+def bsearch(list,search):
+
+#Alston Clark
+
+
+
+
+  top = len(list)
+
+  bottom = 0
+
+  
+ 
+
+  if (top >= search >= bottom):
+    return -1 
+
+   
+  while ( bottom != top):
+    # when bottom == top then you have found the index
+
+    mid = (top + bottom) / 2
+
+    if (search < list[mid]):
+      top = mid - 1
+
+
+    elif ( search > list[mid]):
+      bottom = mid + 1
+
+
+    else:
+      return mid
