dcc4105707c5ed47e5ba1d66d1a0bd5ea929f0ca
gottkris <paakasut@yahoo.com>
2013-11-17 13:48:07 -0500

    Added Chris Quist's binary function to team r3-Bo0t's file

--- projects/bsearch/r3-Bo0tbsearch.py
+++ projects/bsearch/r3-Bo0tbsearch.py
@@ -6,3 +6,22 @@
 	Brittany Miller
 	Eric Walker '''
 
+
+
+
+# Chris Quist's binary search
+def bsearch(wlist, item):
+    beginning=0
+    end = len(wlist)
+    while beginning!=end:
+        middle = (beginning + end)/2
+        if item == wlist[ middle]:
+            return middle
+        elif item < wlist[middle]:
+            end = middle
+        else:
+            if beginning == middle:
+                return -1
+            else:
+                beginning = middle
+    return -1
