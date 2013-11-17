da608be028e9bff356bb25a2faa24c75947d8da7
travon-speller <travon.speller@bison.howard.edu>
2013-11-17 16:42:47 -0500

    Update CoolKids-bsearch.py

--- projects/bsearch/CoolKids-bsearch.py
+++ projects/bsearch/CoolKids-bsearch.py
@@ -12,17 +12,17 @@ Christian Quist
 Solutions:
 
 
-def bSearch(searchList, target):
-   low = 0
-   high = len(searchList)
-   while low <= high:
-      if len(searchList)==0:
-         return "EMPTY LIST"
-      testpos = low + (high-low)/2
-      ITEM = searchList[testpos]
-      if ITEM == target:
-         return testpos            
-      elif ITEM < target: 
-         low = testpos+1
-      else:
-         high = testpos-1
+# def bSearch(searchList, target):
+#    low = 0
+#    high = len(searchList)
+#    while low <= high:
+#       if len(searchList)==0:
+#          return "EMPTY LIST"
+#       testpos = low + (high-low)/2
+#       ITEM = searchList[testpos]
+#       if ITEM == target:
+#          return testpos            
+#       elif ITEM < target: 
+#          low = testpos+1
+#       else:
+#          high = testpos-1
