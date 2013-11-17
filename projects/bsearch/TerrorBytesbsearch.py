b3fb7e6b9a8b7b5d69f6e536591794567c45bbf6
Jaxter12 <jonathon.moody@bison.howard.edu>
2013-11-17 18:30:01 -0500

    Adjusted bsearch function indentation.

--- projects/bsearch/TerrorBytesbsearch.py
+++ projects/bsearch/TerrorBytesbsearch.py
@@ -46,22 +46,25 @@ print binary_search([1,2,3],3)
     
 #Jonathon Moody
     
-    def bsearch(listm,element):
-          Lo =0
-          Hi = len(listm)
-          if Hi == 0:
-              return -1
-          while Lo < Hi:
-                  mid = (Lo+Hi)/2          #Have to find the midpoint first
-                  midvalue = listm[mid]
-                  if midvalue < element:       #if the value is less than the wanted value the function will search higher along the list
-                      Lo = mid + 1
-                  elif midvalue > element:     #if the value is greater than the wanted value the function will search lower along the list.
-                      Hi = mid
-                  elif midvalue == element:
-                      return mid   #Once midvalue equals the wanted value the function will return it.
-                  else:
-                      return -1
+def bsearch(listm,element):
+    Lo = 0
+    Hi = len(listm)
+    if Hi == 0:
+        return -1
+    while Lo < Hi:
+        mid = (Lo+Hi)/2          #Have to find the midpoint first
+        midvalue = listm[mid]
+        if midvalue < element:       #if the value is less than the wanted value the function will search higher along the list
+            Lo = mid + 1
+        elif midvalue > element:     #if the value is greater than the wanted value the function will search lower along the list.
+            Hi = mid
+        elif midvalue == element:
+                return mid   #Once mid equals the wanted value the function will return it.
+        else:
+            return -1
 
 
+a = [1,3,2,4,6,7,8,9]
+
+print bsearch(a,4)
 
