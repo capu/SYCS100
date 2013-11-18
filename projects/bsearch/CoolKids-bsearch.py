1a9d37080844231c3cef694d04ff26943843e476
Glenisha Smith <Glenisha.smith@bison.howard.edu>
2013-11-18 15:55:22 -0500

    Final Update
    Update with test code removed

--- projects/bsearch/CoolKids-bsearch.py
+++ projects/bsearch/CoolKids-bsearch.py
@@ -27,30 +27,18 @@ Solutions:
 #       else:
 #          high = testpos-1
 #Glenisha Smith @02679391
-# November 17, 2013
-# SYCS 100
-# Binary Search
-
-
-
 def bsearch(myItem, myList):
 	found = False
 	low = 0
 	high = len(myList)-1
-	while low <= high and not found:
+	while low <= high:
 		mid = (low + high)//2
 		if myList[mid]== myItem:
 			found = True
+			return mid
 		elif myList[mid] < myItem:
 			low = mid + 1
 		else :
 			high = mid - 1
-	return found
+	return -1
 
-List = sorted([67,78,9,67,53,24,23,87])
-Item = 7
-isitFound = bsearch(Item, List)
-if isitFound:
-	print List.index(Item)
-else:
-	print -1
