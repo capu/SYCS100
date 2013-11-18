b866426bb90846fce9803372ffb56397ac55ef6c
Glenisha Smith <Glenisha.smith@bison.howard.edu>
2013-11-18 17:43:22 -0500

    Update
    Final submission:
    rearranged bsearch parameters, removed "found" variable, and commented my code

--- projects/bsearch/CoolKids-bsearch.py
+++ projects/bsearch/CoolKids-bsearch.py
@@ -27,18 +27,22 @@ Solutions:
 #       else:
 #          high = testpos-1
 #Glenisha Smith @02679391
-def bsearch(myItem, myList):
-	found = False
-	low = 0
-	high = len(myList)-1
-	while low <= high:
-		mid = (low + high)//2
-		if myList[mid]== myItem:
-			found = True
-			return mid
-		elif myList[mid] < myItem:
-			low = mid + 1
+def bsearch(myList, myItem):
+	low = 0 #beginning of list
+	high = len(myList)-1 #end of list "-1" is needed or you can run into "index out of range" error
+	while low <= high: 
+		mid = (low + high)//2 #mid is the index of half way position in your list
+		if myList[mid]== myItem: #checks if the item is  located at middle index
+			return mid # exits the while loop and returns index of item
+		elif myList[mid] < myItem: 
+			low = mid + 1 # the mid object is less than the item, the new low becomes 1 index higher
 		else :
-			high = mid - 1
-	return -1
+			high = mid - 1 #if mid is > item the high index bcomes the mid index minus 1
+	return -1 #returns -1 if object is not in the list
+
+
+
+	
+
+
 
