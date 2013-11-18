2c9355873a20e6cb2e8ac70b92a8d143a34381c5
Selina <selina.jones@bison.howard.edu>
2013-11-18 18:12:21 -0500

    Update menjosbsearch.py
    Took out all temp code

--- projects/bsearch/menjosbsearch.py
+++ projects/bsearch/menjosbsearch.py
@@ -21,3 +21,26 @@ def bsearch( alist , element):
 
 	return -1
 #Maimuna Ahmed's code ends hur
+#Selina Jones git-jayenator565 bsearch code
+
+
+def bsearch(mylist,x):
+    mylist.sort()	
+    size=len(mylist)
+    low=0
+    high=size
+    if mylist==[]:
+        return mylist
+    if high==size:
+        high =len(mylist)-1
+    while low<=high:
+        mid = (low+high)/2
+        midval=mylist[mid]
+        if midval<x:
+            low=mid+1
+        elif midval>x:
+            high=mid
+        else:
+            return mid
+    return -1
+
